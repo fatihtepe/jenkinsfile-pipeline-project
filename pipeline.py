0575c49723826d2b9b86a25616ebc597378c62ce
Fatih TEPE, Ed.D <79118602+fatihtepe@users.noreply.github.com>
2021-10-05 21:01:24 -0400

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,3 +1,4 @@
 print('My first python job which is run within Jenkinsfile.')
 
 print('My first python job which is run within Jenkinsfile.')
+print('My first python job which is run within Jenkinsfile.')
